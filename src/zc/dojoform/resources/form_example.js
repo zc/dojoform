298f16ba97b0442413b5fd41458d8ab3de9fc3b4
Alex Chapman <achapman@zope.com>
2009-06-29 20:23:39 +0000

    Selenium passing again

--- src/zc/dojoform/resources/form_example.js
+++ src/zc/dojoform/resources/form_example.js
@@ -6,6 +6,7 @@ dojo.addOnLoad( function () {
         result = zc.dojo.build_form(form_data,
             dojo.create('div', {}, dojo.body())
         );
+        create_buttons(form_data, result.button_locale);
         result.form_node.startup();
     };
 
@@ -16,4 +17,26 @@ dojo.addOnLoad( function () {
     });
    
     console.log('form loaded');
+
+    create_buttons = function (config, button_locale) {
+        if (config.definition.actions != undefined){
+            actions = config.definition.actions;
+            for (action_index in actions) {
+                action = actions[action_index];
+                var button = new dijit.form.Button({
+                    label: action.label,
+                    id: action.name
+                }, dojo.create('div', {style: "float:left;"}));
+                dojo.connect(button, 'onClick', function () {
+                    zc.dojo.submit_form({
+                        url: action.url,
+                        form_id: config.definition.prefix,
+                        task: 'Submitting Form',
+                    });
+                });
+                button_locale.appendChild(button.domNode);
+            }
+        }
+    }
+
 });
