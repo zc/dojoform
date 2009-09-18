f061e742e501371d5d758146c74979770584d22b
Alex Smith <alex.smith@redhat.com>
2009-09-18 15:22:25 +0000

    remove trailing whitespace.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     author_email = 'jim@zope.com',
     description = '',
     license = 'ZPL 2.1',
-    
+
     packages = find_packages('src'),
     namespace_packages = ['zc'],
     package_dir = {'': 'src'},
