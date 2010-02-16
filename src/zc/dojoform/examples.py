9532616773503fc29697adc77e19baef17c3171f
Satchidanand Haridas <satchit@zope.com>
2010-02-16 23:57:38 +0000

    Fixed selenium tests broken by changes to this package and zc.ajaxform.
    - added some additional tests as well.

--- src/zc/dojoform/examples.py
+++ src/zc/dojoform/examples.py
@@ -7,8 +7,8 @@ class Calculator(zc.ajaxform.calculator_example.Calculator):
 
     def template(self):
         return """<html><head>
-               <style type="text/css">@import "http://o.aolcdn.com/dojo/1.3.0/dijit/themes/tundra/tundra.css";</style>
-               <script type="text/javascript" src="http://o.aolcdn.com/dojo/1.3.0/dojo/dojo.xd.js"></script>
+               <style type="text/css">@import "http://o.aolcdn.com/dojo/1.4.0/dijit/themes/tundra/tundra.css";</style>
+               <script type="text/javascript" src="http://o.aolcdn.com/dojo/1.4.0/dojo/dojo.xd.js"></script>
                <script type="text/javascript" src="/@@/zc.dojoform/zc.dojo.js"></script>
                <script type="text/javascript" src="/@@/zc.dojoform/calculator_example.js"></script>
                </head><body class=tundra></body></html>"""
@@ -18,8 +18,8 @@ class Container(zc.ajaxform.calculator_subapplication_example.Container):
 
     def template(self):
         return """<html><head>
-               <style type="text/css">@import "http://o.aolcdn.com/dojo/1.3.0/dijit/themes/tundra/tundra.css";</style>
-               <script type="text/javascript" src="http://o.aolcdn.com/dojo/1.3.0/dojo/dojo.xd.js"></script>
+               <style type="text/css">@import "http://o.aolcdn.com/dojo/1.4.0/dijit/themes/tundra/tundra.css";</style>
+               <script type="text/javascript" src="http://o.aolcdn.com/dojo/1.4.0/dojo/dojo.xd.js"></script>
                <script type="text/javascript" src="/@@/zc.dojoform/zc.dojo.js"></script>
                <script type="text/javascript" src="/@@/zc.dojoform/container_example.js"></script>
                </head><body class=tundra></body></html>"""
@@ -30,8 +30,12 @@ class Form(zc.ajaxform.form_example.FormExample):
     def template(self):
         return """<html><head>
                <style type="text/css">
-               @import "http://o.aolcdn.com/dojo/1.3.0/dijit/themes/tundra/tundra.css";</style>
-               <script type="text/javascript" src="http://o.aolcdn.com/dojo/1.3.0/dojo/dojo.xd.js"></script>
+               @import "http://o.aolcdn.com/dojo/1.4.0/dijit/themes/tundra/tundra.css";</style>
+
+               <style type="text/css" media="all">
+               @import url("http://o.aolcdn.com/dojo/1.4.0/dojox/grid/enhanced/resources/tundraEnhancedGrid.css");
+               </style>
+               <script type="text/javascript" src="http://o.aolcdn.com/dojo/1.4.0/dojo/dojo.xd.js"></script>
                <script type="text/javascript" src="/@@/zc.dojoform/zc.dojo.js"></script>
                <script type="text/javascript" src="/@@/zc.dojoform/form_example.js"></script>
                </head><body class=tundra></body></html>"""
