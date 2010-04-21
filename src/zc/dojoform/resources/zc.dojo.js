1ee081b1c89625fcd43b883c8e70d91ccc962955
Alex Chapman <achapman@zope.com>
2010-04-21 18:31:05 +0000

    - adjust form for smaller monitors

--- src/zc/dojoform/resources/zc.dojo.js
+++ src/zc/dojoform/resources/zc.dojo.js
@@ -377,6 +377,7 @@ function build_record_form(widget_name, grid, index_map) {
     });
     var rec_form = new dijit.form.Form({
         method: 'POST',
+        style: 'max-height: 400px; overflow: auto;',
         encType: 'multipart/form-data'
     }, dojo.create('div', null, edit_dlg.domNode));
     var record_input = new dijit.form.TextBox({
