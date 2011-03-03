abe13b8fddf4c05a1823145ee86de26b6eb2b902
Jim Fulton <jim@zope.com>
2011-03-03 23:00:06 +0000

    Got rid of post-startup and the fit business. It's not needed afaict.
    Moved the subscriber definitions outside of the constructor.

--- src/zc/dojoform/ckeditor/resources/widget.js
+++ src/zc/dojoform/ckeditor/resources/widget.js
@@ -18,61 +18,54 @@ var CKEditorWidget = function (config, parent, order) {
         parent
     );
     textarea.value = config.value || '';
-    parent.postStartup = function (formNode) {
-        var editor;
-        var ckeditorConfig = config.ckConfig || {};
-        if (order !== null) {
-            ckeditorConfig.tabIndex = order;
-        }
-        if (ckeditorCustomConfig) {
-            ckeditorConfig.customConfig = ckeditorCustomConfig;
-        }
-        editor = CKEDITOR.replace(textarea, ckeditorConfig);
-        var handler = function () {
-            textarea.value = editor.getData();
-        };
-        window.addEventListener('beforeSubmit', handler, true);
-        zc.dojo.GLOBAL_HANDLERS.push(handler);
-        dojo.subscribe(zc.dojo.recordFormSubmittedTopic, handler);
-        CKEDITOR.on('instanceReady', function (event) {
-            if (formNode.fit) {
-                formNode.fit();
-            }
-        });
+    var editor;
+    var ckeditorConfig = config.ckConfig || {};
+    if (order !== null) {
+        ckeditorConfig.tabIndex = order;
+    }
+    if (ckeditorCustomConfig) {
+        ckeditorConfig.customConfig = ckeditorCustomConfig;
+    }
+    editor = CKEDITOR.replace(textarea, ckeditorConfig);
+    var handler = function () {
+        textarea.value = editor.getData();
     };
+    window.addEventListener('beforeSubmit', handler, true);
+    zc.dojo.GLOBAL_HANDLERS.push(handler);
+    dojo.subscribe(zc.dojo.recordFormSubmittedTopic, handler);
+    return parent;
+};
 
-    /* subscribers to reset/set/save photo widget data.
-    */
-    if (!zc.dojo.widgets['zc.dojoform.ckeditor.CKEditor'].subscribers) {
-        dojo.subscribe(zc.dojo.beforeRecordFormSubmittedTopic, function(frm_id) {
-            dojo.forEach(dojo.query('textarea', frm_id), function (textarea) {
+zc.dojo.widgets['zc.dojoform.ckeditor.CKEditor'] = CKEditorWidget;
+
+dojo.subscribe(
+    zc.dojo.beforeRecordFormSubmittedTopic, function(frm_id) {
+        dojo.forEach(
+            dojo.query('textarea', frm_id), function (textarea) {
                 var editor = CKEDITOR.instances[textarea.name];
                 if (editor) {
                     textarea.value = editor.getData();
                 }
             });
-        });
-        dojo.subscribe(zc.dojo.dialogFormResetTopic, function(frm_id) {
-            dojo.forEach(dojo.query('textarea', frm_id), function (textarea) {
+    });
+dojo.subscribe(
+    zc.dojo.dialogFormResetTopic, function(frm_id) {
+        dojo.forEach(
+            dojo.query('textarea', frm_id), function (textarea) {
                 var editor = CKEDITOR.instances[textarea.name];
                 if (editor) {
                     editor.setData('');
                 }
             });
-        });
-        dojo.subscribe(zc.dojo.dialogFormUpdateTopic, function(frm_id, row) {
-            dojo.forEach(dojo.query('textarea', frm_id), function (textarea) {
+    });
+dojo.subscribe(
+    zc.dojo.dialogFormUpdateTopic, function(frm_id, row) {
+        dojo.forEach(
+            dojo.query('textarea', frm_id), function (textarea) {
                 textarea.value = row[textarea.name];
                 var editor = CKEDITOR.instances[textarea.name];
                 if (editor) {
                     editor.setData(row[textarea.name]);
                 }
             });
-        });
-        zc.dojo.widgets['zc.dojoform.ckeditor.CKEditor'].subscribers = true;
-    }
-
-    return parent;
-};
-
-zc.dojo.widgets['zc.dojoform.ckeditor.CKEditor'] = CKEditorWidget;
+    });
