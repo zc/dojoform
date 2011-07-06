aca360b21ee43de5d7f242a2f8e070d15212e7c4
Alex Chapman <achapman@zope.com>
2011-07-06 20:14:33 +0000

    - fix recordlist widget to respect order

--- src/zc/dojoform/resources/zc.dojo.js
+++ src/zc/dojoform/resources/zc.dojo.js
@@ -1722,6 +1722,17 @@ dojo.ready(
                                 grid.store.save();
                             }
                         }, dojo.create('div', null, this.domNode));
+                        // While the event seems deceptively decorative, we are
+                        // using this to know when to recalculate our grid
+                        // value, since we do so by iterating over rows.
+                        // We do this because order is important, and the store
+                        // cares nothing of order.
+                        dojo.connect(grid, 'postresize', dojo.hitch(this,
+                            function (item) {
+                                this._set_inputs();
+                                this.onChange(this.attr('value'));
+                            })
+                        );
                     }
 
                     this.startup = function (node) {
@@ -1736,29 +1747,16 @@ dojo.ready(
                     dojo.connect(
                         this.grid, 'startup', dojo.hitch(
                             this, function () {
-                                this._set_inputs(this.get('value'));
+                                this._set_inputs();
                             }));
                 },
 
+                onChange: function (value) {
+                    this.inherited(arguments);
+                },
+
                 _getValueAttr: function () {
-                    var i = 0;
-                    var content = {};
-                    dojo.forEach(this.grid.store._arrayOfAllItems,
-                        dojo.hitch(this, function (item) {
-                            if (item === null) {
-                                return;
-                            }
-                            dojo.forEach(this.rc.widgets, dojo.hitch(this,
-                                function (def) {
-                                    var name = this.config.name + '.' +
-                                        def.name;
-                                    if (item[name] !== undefined) {
-                                        content[name + '.' + i] = item[name][0];
-                                    }
-                            }));
-                            i++;
-                    }));
-                    return dojo.toJson(content);
+                    return this.input_value.value;
                 },
 
                 _values_from: function (value) {
@@ -1781,40 +1779,41 @@ dojo.ready(
                     };
                     var store = new dojo.data.ItemFileWriteStore(
                         {data: data});
-                    dojo.connect(store, 'onNew', dojo.hitch(this,
-                        this._on_change));
-                    dojo.connect(store, 'onDelete', dojo.hitch(this,
-                        this._on_change));
-                    dojo.connect(store, 'onSet', dojo.hitch(this,
-                        this._on_change));
+                    // This won't trigger a resize, so we have to keep this
+                    // event.
+                    dojo.connect(store, 'onSet', dojo.hitch(this, function () {
+                        this._set_inputs();
+                    }));
                     return store;
                 },
 
-                _set_inputs: function (value) {
-                    this.input_value.value = value;
+                _set_inputs: function () {
                     this.input_parent.innerHTML = '';
-                    var values = dojo.fromJson(value);
-                    for (name in values) {
-                        dojo.create('input', {
-                            type: 'hidden',
-                            name: name,
-                            value: values[name]
-                        }, this.input_parent);
+                    var items = {};
+                    for (var i=0; i< this.grid.rowCount; i++) {
+                        var rec = this.grid.getItem(i);
+                        if (rec) {
+                            var attrs = this.grid.store.getAttributes(rec);
+                            dojo.forEach(attrs, dojo.hitch(this, function (attr) {
+                                if (attr != 'name') {
+                                    var name = attr+ '.'+String(i);
+                                    var value = this.grid.store.getValue(rec,
+                                        attr);
+                                    dojo.create('input', {
+                                        type: 'hidden',
+                                        name: name,
+                                        value: value
+                                    }, this.input_parent);
+                                    items[name] = value;
+                                }
+                            }));
+                        }
                     }
-                },
-
-                onChange: function (value) {
-                    this._set_inputs(value);
-                    this.inherited(arguments);
-                },
-
-                _on_change: function () {
-                    this.onChange(this.attr('value'));
+                    this.input_value.value = dojo.toJson(items);
                 },
 
                 _setValueAttr: function (value) {
                     this.grid.setStore(this._store_from_data(value));
-                    this.onChange(this.get('value'));
                 },
 
                 isValid: function () {
